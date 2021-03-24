4442e444073b16c160804ed8043987869d97c98a
brunelesqui <44075743+brunelesqui@users.noreply.github.com>
2021-03-24 12:08:41 -0300

    Create keys.h

---
+++ src/keys.h
@@ -0,0 +1,15 @@
+/* keys.h contiene definiciones para los códigos de las teclas */
+
+#ifndef KEYS_H
+#define KEYS_H
+
+#define UP_KEY    72
+#define DOWN_KEY  80
+#define LEFT_KEY  75
+#define RIGHT_KEY 77
+
+#define ESC_KEY   27
+#define ENTER_KEY 13
+#define DEL_KEY    8
+
+#endif
